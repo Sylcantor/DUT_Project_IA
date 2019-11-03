38634796e7851271c7f2a49f9e66084f0e921057
Caliminou <45433718+Caliminou@users.noreply.github.com>
2019-11-03 14:57:18 +0100

    observer

---
+++ Jeux - Exemples/Frozen Lake - QLearning/part1/QDiagram.py
@@ -0,0 +1,15 @@
+# affichage
+import matplotlib.pyplot as plt
+
+
+class QDiagram:
+
+    def __init__(self, cumul_reward_list):
+        self.cumul_reward_list = cumul_reward_list
+        self.generate_affi()
+
+    def generate_affi(self):
+        plt.plot(self.cumul_reward_list[:100])
+        plt.ylabel('Cumulative reward')
+        plt.xlabel('Étape')
+        plt.show()
