8f109a39040fabc37c44ebfb3c622a4a7701f620
Caliminou <45433718+Caliminou@users.noreply.github.com>
2019-11-03 11:14:06 +0100

    affichage

--- Jeux/Frozen Lake - QLearning/part1/QLearning.py
+++ Jeux/Frozen Lake - QLearning/part1/QLearning.py
@@ -13,9 +13,9 @@ actions_n = 4
 Q = np.zeros([states_n, actions_n])
 
 # Set learning parameters
-lr = .85  # learning rate, c’est la vitesse d’apprentissage
-y = .99  # détermine l’importance des récompenses futures
-num_episodes = 1000  # nombre de parties que l’on va faire
+lr = .85                 # learning rate, c’est la vitesse d’apprentissage
+y = .99                  # détermine l’importance des récompenses futures
+num_episodes = 1000      # nombre de parties que l’on va faire
 cumul_reward_list = []
 actions_list = []
 states_list = []
@@ -24,38 +24,39 @@ states_list = []
 game = Game(4, 4, 0.1)
 
 for i in range(num_episodes):  # for jusqu'à la fin du nombre de parties que l’on va faire
-    
-    print("____nouvelle partie____") 
-    
+
+    print("____nouvelle partie____")
+
     actions = []
     s = game.reset()
     states = [s]
     cumul_reward = 0
     d = False
     while True:
-        
-        # on choisit une action aléatoire avec une certaine probabilité, 
+
+        # on choisit une action aléatoire avec une certaine probabilité,
         # qui décroit avec i : 1. / (i +1)
         Q2 = Q[s, :] + np.random.randn(1, actions_n)*(1. / (i + 1))
         a = np.argmax(Q2)
-        
+
+        # on récupère le reward dans le jeu (pas besoins de matrice ici)
         s1, reward, d, _ = game.move(a)
-        
+
         # Fonction de mise à jour de la Q-table
         Q[s, a] = Q[s, a] + lr*(reward + y * np.max(Q[s1, :]) - Q[s, a])
         cumul_reward += reward
         s = s1
         actions.append(a)
         states.append(s)
-        
+
         if d == True:
             break
-        
+
         game.print()  # affichage du jeu en temps réel
-      
-    print("Affichage fin:")    
+
+    print("Affichage fin:")
     game.print()  # affichage du jeu à la fin d'une partie
-        
+
     states_list.append(states)
     actions_list.append(actions)
     cumul_reward_list.append(cumul_reward)
