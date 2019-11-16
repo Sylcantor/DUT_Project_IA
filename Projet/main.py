1d477074437f46ac4dff73c834b1833f73fdbf7c
Caliminou <45433718+Caliminou@users.noreply.github.com>
2019-11-16 20:59:18 +0100

    py

--- Projet/main.py
+++ Projet/main.py
@@ -1,10 +1,8 @@
 
-from Games.FrozenLake import Game
-
-from api.QLearning import QLearning
-
+from Games.FrozenLake.Game import Game
+from api.QLearning.QLearning import QLearning
 
 # Choisissez un jeu
 game = Game(4, 4, 0.1)
 # Choisissez un type d'IA
-q = QLearning(game, 16, 4)
+q = QLearning(game, 16, 4)L
