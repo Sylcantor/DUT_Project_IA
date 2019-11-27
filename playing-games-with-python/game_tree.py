ca802c9ca3ca3b86ee9af412288a327137b6f1fd
castel <aurelien.castel@etu-pec.fr>
2019-11-27 10:13:36 +0000

    l

---
+++ playing-games-with-python/game_tree.py
@@ -0,0 +1,17 @@
+
+def create_tree(self, game, state, win_value, loss_value):
+        """
+        Méthode pour créer un arbre de jeu en fonction de l'état du jeu
+        """
+        
+        game_tree = [[' ', ' ', ' '],
+                     [' ', ' ', ' '],
+                     [' ', ' ', ' ']]
+        
+        
+        
+        return game_tree
+
+def copy_game_state(state):
+    
+    return
