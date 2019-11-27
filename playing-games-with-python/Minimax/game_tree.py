8413a957a97914915692da411a0fe34d76a04d5b
Caliminou <45433718+Caliminou@users.noreply.github.com>
2019-11-27 17:06:50 +0100

    l

--- playing-games-with-python/Minimax/game_tree.py
+++ playing-games-with-python/Minimax/game_tree.py
@@ -1,15 +1,39 @@
 
-def create_tree(self, game, state, win_value, loss_value):
-        """
-        Méthode pour créer un arbre de jeu en fonction de l'état du jeu
-        https://becominghuman.ai/practical-artificial-intelligence-for-game-development-5b0ebf35993b
-        """
-        
-        # Nested List
-        game_tree = ["Happy", [2,0,1,5]]       
-        
-        return game_tree
+from numpy.lib.function_base import append
 
-def copy_game_state(state):
-    
-    return
+
+def create_tree(self, game, state, win_value, loss_value):
+    """
+    Méthode pour créer un arbre de jeu en fonction de l'état du jeu
+    https://becominghuman.ai/practical-artificial-intelligence-for-game-development-5b0ebf35993b
+    """
+
+    # Nested List
+    game_tree = ["Happy", [2, 0, 1, 5]]
+
+    return game_tree
+
+
+class GameState():
+
+    def __init__(self, stateX, stateY):
+        """
+        Constructeur dans lequel on définit
+        le nombre d'allumette
+        """
+        self.state = []
+
+        for i in range(stateY):
+
+            stateRow = []
+
+            for j in range(stateX):
+
+                stateRow.append(' ')
+
+            self.state.append(stateRow)
+
+        print(self.state)
+
+
+GameState(3, 4)
